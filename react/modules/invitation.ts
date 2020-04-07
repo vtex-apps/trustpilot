b61b8180470d449f1e82a68c27752b6a66cd4de4
Christian Kaisermann <christian@kaisermann.me>
2020-04-07 14:23:54 -0300

    Create an invitation in the order placed page

---
+++ react/modules/invitation.ts
@@ -0,0 +1,25 @@
+import { OrderPlacedTrackedData } from '../typings/events'
+
+export function createProductInvitation(eventData: OrderPlacedTrackedData) {
+  const { visitorContactInfo, orderGroup, transactionProducts } = eventData
+
+  const [email, name, lastname] = visitorContactInfo
+  const products = transactionProducts.map(product => {
+    return {
+      sku: product.sku,
+      productUrl: `${window.location.origin}/${product.slug}/p`,
+      name: product.name,
+    }
+  })
+  const skus = products.map(p => p.sku)
+
+  const tpData = {
+    recipientEmail: email,
+    recipientName: `${name} ${lastname}`,
+    referenceId: orderGroup,
+    source: 'InvitationScript',
+    productSkus: skus,
+    products,
+  }
+  window.tp('createInvitation', tpData)
+}
